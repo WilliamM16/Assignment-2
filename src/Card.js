23c023634d06ac4fc28abb2f0d1bede4ab1d2548
William <williammak216@gmail.com>
2022-11-21 15:48:55 -0500

    matching game works and delay implemented

--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,18 @@ import water from "./images/water.jpg";
 import classnames from "classnames";
 import './Card.css';
 
-const Card = ({ card, index}) => {
+const Card = ({ onClick, card, index, inactive, flipped, disabled }) => {
+    const handleClick = () => {
+      !flipped && !disabled && onClick(index);
+    };
+
     return (
         <div
-          className={classnames("card", {})}
+          className={classnames("card", {
+            "flipped": flipped,
+            "inactive": inactive
+          })}
+          onClick={handleClick}
         >
           <div className="cardBack">
             <img src={water} alt="water" />
